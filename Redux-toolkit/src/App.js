b58cb64233c2570eaf5866d5af8a26fad9eea1e4
yiminwook <grs0416@gmail.com>
2023-01-17 00:25:13 +0900

    Feat: dispatch use React redux

--- Redux-toolkit/src/App.js
+++ Redux-toolkit/src/App.js
@@ -1,18 +1,37 @@
 import './App.css';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
-function Counter() {
+function reducer(state, action) {
+  const newState = {...state};
+  switch(action.type) {
+    case "INCREMENT":
+      newState.value += action.step;
+      return newState;
+    default: 
+      return newState;
+  }
+}
+
+const store = createStore(reducer, {value: 0});
+
+function Counter(props) {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>+</button> 0
+      <button onClick={() => dispatch({type: "INCREMENT", step: 2})}>+</button> {count}
     </div>
   )
 }
 
 function App() {
   return (
-    <div className="App">
-      <Counter></Counter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
 
