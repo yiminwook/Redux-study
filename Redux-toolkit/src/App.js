65bca17aeaa0c4f3f5107e78a884bd3c7105bf73
yiminwook <grs0416@gmail.com>
2023-01-17 01:02:21 +0900

    Feat: createSlice

--- Redux-toolkit/src/App.js
+++ Redux-toolkit/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { createStore } from 'redux';
 import { Provider, useSelector, useDispatch } from 'react-redux';
+import { createSlice , configureStore } from '@reduxjs/toolkit';
 
+/*
 function reducer(state, action) {
   const newState = {...state};
   switch(action.type) {
@@ -14,13 +16,33 @@ function reducer(state, action) {
 }
 
 const store = createStore(reducer, {value: 0});
+*/
+
+const counterSlice = createSlice({
+  name: 'counter', //이름
+  initialState: {value: 0}, //초기값
+  reducers: {
+    INCREMENT: (state, action) => {
+      state.value += action.payload.step; //toolkit을 쓰면 불변성을 지키지 않아도 됌
+      return state;
+    } 
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer //복수형 쓰면 안된다.
+  }
+});
 
 function Counter(props) {
-  const count = useSelector((state) => state.value);
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  console.log(counterSlice.actions)
   return (
     <div>
-      <button onClick={() => dispatch({type: "INCREMENT", step: 2})}>+</button> {count}
+      {/* <button onClick={() => dispatch({type:'counter/INCREMENT', step: 2})}>+</button> {count} */}
+      <button onClick={() => dispatch(counterSlice.actions.INCREMENT({step: 2}))}>+</button> {count}
     </div>
   )
 }
