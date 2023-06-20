81e0c1b96395b8408c434df664a8c721247cac18
yiminwook <grs0416@gmail.com>
2023-06-20 23:50:22 +0900

    Feat: useDispatch, useSelector type 재지정

---
+++ Zerocho/react-redux/src/redux/store.ts
@@ -0,0 +1,36 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import firstMiddlware from "@/redux/middlewares/first";
+import thunkMiddeware from "@/redux/middlewares/thunk";
+import reducer from "@/redux/reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
+
+const initialState: Record<string, any> = {
+  user: {
+    data: null,
+    isLoggin: false,
+  },
+  posts: [],
+  comments: [],
+  favarites: [],
+  history: [],
+  likes: [],
+  followers: [],
+};
+
+const enhancer = compose(
+  applyMiddleware(firstMiddlware, thunkMiddeware),
+  composeWithDevTools()
+);
+
+const store = createStore(reducer, initialState, enhancer);
+
+const useStoreDispatch = () =>
+  useDispatch<ThunkDispatch<typeof initialState, unknown, AnyAction>>();
+
+const useStoreSelector: TypedUseSelectorHook<typeof initialState> = useSelector;
+
+export default store;
+export { useStoreDispatch as useDispatch, useStoreSelector as useSelector };
