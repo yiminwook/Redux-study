70bd3e2edc576cad5a0969db7be349455d1a8c03
yiminwook <grs0416@gmail.com>
2023-06-20 23:48:26 +0900

    Update: fileName

--- Zerocho/second/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const { combineReducers } = require("redux");
-const postReducer = require("./post");
-const userReducer = require("./user");
-
-module.exports = combineReducers({
-  user: userReducer,
-  posts: postReducer,
-});
