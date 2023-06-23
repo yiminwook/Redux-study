3143b93364b36d8f6a1aa1a78ea1a21a02054a61
yiminwook <grs0416@gmail.com>
2023-06-23 18:32:29 +0900

    Init: react-mobx-hook project

---
+++ Zerocho/react-mobx/config-overrides.js
@@ -0,0 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+const { alias, configPaths } = require("react-app-rewire-alias");
+const { override, useBabelRc } = require("customize-cra");
+
+module.exports = override(
+  useBabelRc(),
+  alias(configPaths("./tsconfig.paths.json"))
+);
